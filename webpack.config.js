79926b4f459e83e4445d002c095cae1869ed29e3
Karla <karlaabdraeva@icloud.com>
2025-06-13 01:19:19 +0300

    доделала стили hero, навигацию

--- webpack.config.js
+++ webpack.config.js
@@ -3,57 +3,51 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
         test: /\.pug$/,
         use: [
           {
-            loader: 'html-loader'
+            loader: "html-loader"
           },
           {
-            loader: 'pug-plain-loader'
+            loader: "pug-plain-loader"
           }
-        ],
+        ]
       },
       {
-        test: /\.scss$/,
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       }
-    ],
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/templates/index.pug'
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: "src/fonts", to: "fonts" },
-        { from: "src/images", to: "images" }
-      ],
-    }),
+      template: "./src/templates/index.pug",
+      filename: "index.html"
+    })
   ],
   devServer: {
     static: {
       directory: path.join(__dirname, "dist")
     },
     compress: true,
-    port: 3000,
+    port: 3001,
     open: true
   },
   mode: "development"
