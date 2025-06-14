b9bb0e944c16fa324969dc9f89c51608566c67ea
Karla <karlaabdraeva@icloud.com>
2025-06-14 17:13:26 +0300

    сделала анимацию блока hero

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,8 @@ const logoTextElement = headerElement?.querySelector('logo__text') as HTMLElemen
 const logoElement = headerElement?.querySelector('.logo') as HTMLElement | null;
 const heroElement = document.querySelector('.hero') as HTMLElement | null;
 const heroTitleElement = heroElement?.querySelector('.hero__title') as HTMLElement | null;
-const heroSubtitlelement = heroElement?.querySelector('hero__subtitle') as HTMLElement | null;
+const heroTitleLinesElement = heroElement?.querySelectorAll('.hero__line');
+const heroSubtitleElement = heroElement?.querySelector('.hero__subtitle') as HTMLElement | null;
 const heroImageContainerElement = heroElement?.querySelector('hero__image-container') as HTMLElement | null;
 const heroImgElement = heroElement?.querySelector('hero__img') as HTMLElement | null;
 const conceptElement = document.querySelector('.concept') as HTMLElement | null;
@@ -23,3 +24,31 @@ const supportImgElement = supportElement?.querySelector('.support__img') as HTML
 const supportLinkIconElement = supportElement?.querySelector('.support__link-icon') as HTMLElement | null;
 const supportLinkTextElement = supportElement?.querySelector('.support__link-text') as HTMLElement | null;
 
+if (heroTitleLinesElement) {
+  gsap.set(heroTitleLinesElement, { opacity: 0, y: 50 });
+}
+
+if (heroTitleLinesElement) {
+gsap.to(heroTitleLinesElement, {
+  opacity: 1,
+  y: 0,
+  duration: 1,
+  ease: "power3.out",
+  stagger: 0.15,
+  delay: 0.3,
+});
+}
+
+if (heroSubtitleElement) {
+  gsap.fromTo(
+    heroSubtitleElement,
+    { opacity: 0, y: 30 },
+    {
+      opacity: 0.8,
+      y: 0,
+      duration: 1.1,
+      ease: "power2.out",
+      delay: 1
+    }
+  );
+}
