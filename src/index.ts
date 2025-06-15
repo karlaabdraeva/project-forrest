61dfb659c30e3cfccfca5c64173102fbe6a70067
Karla <karlaabdraeva@icloud.com>
2025-06-15 16:59:19 +0300

    завершила анимацию

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ const conceptElement = document.querySelector('.concept') as HTMLElement | null;
 const conceptTitleLines = conceptElement?.querySelectorAll('.concept__line') ?? [];
 const triggerElements = document.querySelectorAll('[data-theme-trigger]');
 const sectionLightElement = document.querySelector('.section-light') as HTMLElement | null;
-const conceptTitleElement = conceptElement?.querySelector('.concept__title') as HTMLElement | null;
+const conceptTitleElements = conceptElement?.querySelectorAll('.concept__title')
 const conceptAccentElement = conceptElement?.querySelector('.concept__title-accent') as HTMLElement | null;
 const conceptTextElement = conceptElement?.querySelector('.concept__title-text') as HTMLElement | null;
 const conceptDescriptionElement = conceptElement?.querySelector('.concept__title-description') as HTMLElement | null;
@@ -32,6 +32,7 @@ const supportElement = document.querySelector('.support') as HTMLElement | null;
 const supportTitleElement = supportElement?.querySelector('.support__title') as HTMLElement | null;
 const supportSubtitleElement = supportElement?.querySelector('.support__subtitle') as HTMLElement | null;
 const supportImgElement = supportElement?.querySelector('.support__img') as HTMLElement | null;
+const supportLinkElement = supportElement?.querySelector('.support__link') as HTMLElement | null;
 const supportLinkIconElement = supportElement?.querySelector('.support__link-icon') as HTMLElement | null;
 const supportLinkTextElement = supportElement?.querySelector('.support__link-text') as HTMLElement | null;
 
@@ -39,31 +40,35 @@ if (heroTitleElement) {
   gsap.set(heroTitleElement, { opacity: 0, y: 100 });
 }
 
-gsap.to(heroTitleElement, {
-  scrollTrigger: {
-    trigger: heroElement,
-    start: 'top 85%',
-  },
+gsap.fromTo(heroTitleElement,
+  { opacity: 0, y: 100 },
+  {
   y: 0,
   opacity: 1,
   duration: 1.5,
   ease: 'power3.out',
-  stagger: 0.15
+  stagger: 0.15,
+  scrollTrigger: {
+    trigger: heroElement,
+    start: 'top 85%',
+    toggleActions: 'play reset play reset'
+  }
 });
 
 
 gsap.fromTo(heroSubtitleElement,
   { opacity: 0, y: 80 },
   {
-    scrollTrigger: {
-      trigger: heroElement,
-      start: 'top 80%',
-    },
     opacity: 0.8,
     y: 0,
     duration: 1.5,
     ease: 'power2.out',
-    delay: 0
+    delay: 0,
+    scrollTrigger: {
+      trigger: heroElement,
+      start: 'top 80%',
+      toggleActions: 'play reset play reset'
+    }
   }
 );
 
@@ -106,36 +111,112 @@ if (heroElement && heroOverlayElement && heroTitleElement && heroSubtitleElement
   });
 }
 
-if (conceptElement && conceptTitleLines.length && conceptTextElement && conceptDescriptionElement) {
+if (conceptElement && conceptTitleElements && conceptTitleLines.length && conceptTextElement && conceptDescriptionElement) {
 
-  gsap.set(conceptTitleLines, { y: '100%', opacity: 0 });
+  gsap.set(conceptTitleElements, { y: '100%', opacity: 0 });
 
   // Анимация появления строк заголовка
-  gsap.to(conceptTitleLines, {
-    scrollTrigger: {
-      trigger: conceptElement,
-      start: 'top 80%',
-    },
+  gsap.fromTo(conceptTitleElements,
+    { y: '100%', opacity: 1 },
+    {
     y: 0,
     opacity: 1,
     duration: 1.2,
     ease: 'power3.out',
-    stagger: 0.15
+    stagger: 0.15,
+    scrollTrigger: {
+      trigger: conceptElement,
+      start: 'top 80%',
+      toggleActions: 'play reset play reset',
+    }
   });
 
-  gsap.fromTo(
-    [conceptTextElement, conceptDescriptionElement],
-    { opacity: 0, y: 40 },
+  gsap.fromTo(conceptTitleLines,
+    { y: '80%', opacity: 1 },
     {
-      scrollTrigger: {
-        trigger: conceptElement,
-        start: 'top 85%',
-      },
       opacity: 1,
       y: 0,
       duration: 1.5,
       ease: 'power2.out',
-      delay: 0.5
+      delay: 0,
+      scrollTrigger: {
+        trigger: conceptElement,
+        start: 'top 85%',
+        toggleActions: 'play reset play reset',
+      }
+    }
+  );
+}
+
+if (supportTitleElement) {
+  gsap.set(supportTitleElement, { opacity: 0, y: 100 });
+}
+
+gsap.fromTo(supportTitleElement,
+  { opacity: 0, y: 100 },
+  {
+  y: 0,
+  opacity: 1,
+  duration: 1.5,
+  ease: 'power3.out',
+  stagger: 0.15,
+  scrollTrigger: {
+    trigger: supportElement,
+    start: 'top 85%',
+    toggleActions: 'play reset play reset'
+  }
+});
+
+
+gsap.fromTo(supportSubtitleElement,
+  { opacity: 0, y: 80 },
+  {
+    opacity: 0.8,
+    y: 0,
+    duration: 1.5,
+    ease: 'power2.out',
+    delay: 0,
+    scrollTrigger: {
+      trigger: supportElement,
+      start: 'top 80%',
+      toggleActions: 'play reset play reset'
+    }
+  }
+);
+
+gsap.fromTo(supportLinkElement,
+  { opacity: 0, y: 100 },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: 'power2.out',
+    delay: 0,
+    scrollTrigger: {
+      trigger: supportElement,
+      start: 'top 80%',
+      toggleActions: 'play reset play reset'
+    }
+  }
+);
+
+if (supportImgElement) {
+  gsap.fromTo(
+    supportImgElement,
+    {
+      opacity: 0,
+      scale: 1.1
+    },
+    {
+      opacity: 1,
+      scale: 1,
+      duration: 1.6,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: '.support',
+        start: 'top 80%',
+        toggleActions: 'play reset play reset',
+      }
     }
   );
 }
